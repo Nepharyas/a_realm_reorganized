a217b9e790cf0d187a69d6eb542ef5f993710708
Nepharyas <280159163+Nepharyas@users.noreply.github.com>
2026-04-30 12:48:17 +0200

    wire window + services into plugin entry

--- ARealmReorganized/Plugin.cs
+++ ARealmReorganized/Plugin.cs
@@ -1,4 +1,8 @@
+using ARealmReorganized.Logic;
+using ARealmReorganized.Services;
+using ARealmReorganized.UI;
 using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
 
 namespace ARealmReorganized;
@@ -9,6 +13,12 @@ public sealed class Plugin : IDalamudPlugin
     private const string MainCommand = "/arr";
 
     public Configuration Config { get; }
+    public WindowSystem Windows { get; } = new("ARealmReorganized");
+    public MainWindow MainWindow { get; }
+
+    public ICabinetService Cabinet { get; }
+    public IGlamourDresserService Dresser { get; }
+    public ArmoireEligibility Eligibility { get; }
 
     public Plugin(IDalamudPluginInterface pi)
     {
@@ -16,19 +26,33 @@ public sealed class Plugin : IDalamudPlugin
 
         Config = Service.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
+        Cabinet = new StubCabinetService();
+        Dresser = new StubGlamourDresserService();
+        Eligibility = new ArmoireEligibility();
+
+        MainWindow = new MainWindow(this);
+        Windows.AddWindow(MainWindow);
+
         Service.CommandManager.AddHandler(MainCommand, new CommandInfo(OnCommand)
         {
             HelpMessage = "Open A Realm Reorganized.",
         });
+
+        Service.PluginInterface.UiBuilder.Draw += Windows.Draw;
+        Service.PluginInterface.UiBuilder.OpenMainUi += OpenMain;
+        Service.PluginInterface.UiBuilder.OpenConfigUi += OpenMain;
     }
 
     public void Dispose()
     {
+        Service.PluginInterface.UiBuilder.Draw -= Windows.Draw;
+        Service.PluginInterface.UiBuilder.OpenMainUi -= OpenMain;
+        Service.PluginInterface.UiBuilder.OpenConfigUi -= OpenMain;
         Service.CommandManager.RemoveHandler(MainCommand);
+        Windows.RemoveAllWindows();
+        MainWindow.Dispose();
     }
 
-    private void OnCommand(string _, string __)
-    {
-        Service.Log.Information("not yet implemented");
-    }
+    private void OpenMain() => MainWindow.IsOpen = true;
+    private void OnCommand(string _, string __) => MainWindow.Toggle();
 }
