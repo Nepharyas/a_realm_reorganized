d29b26de7398483ba2c2d19266999955f7146194
Nepharyas <280159163+Nepharyas@users.noreply.github.com>
2026-05-08 16:03:24 +0200

    always open logs after apply, not only in dry-run

--- ARealmReorganized/UI/SettingsWindow.cs
+++ ARealmReorganized/UI/SettingsWindow.cs
@@ -23,7 +23,6 @@ public sealed class SettingsWindow : Window, IDisposable
 
     public void OpenOnLogs()
     {
-        if (!plugin.Config.DryRun) return;
         IsOpen = true;
         jumpToLogs = true;
     }
